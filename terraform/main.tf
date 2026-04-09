dfc1959575253e292a6146f02d70bd45c4aa8695
Noam-Journo <noamjorno6@gmail.com>
2026-04-10 01:21:48 +0300

    added providers10

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ module "ecr" {
 }
 module "app_secrets" {
   source      = "./modules/secrets"
-  secret_name = "${var.cluster_name}-db-pass-v14"
+  secret_name = "${var.cluster_name}-db-pass-v15"
   tags        = local.common_tags
 }
 module "kubernetes" {
