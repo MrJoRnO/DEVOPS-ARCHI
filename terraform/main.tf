29c42f5a370fc80105f04961832ef9608a8f8dc2
Noam-vm <noamjorno@gmail.com>
2026-04-13 19:50:54 +0300

    fix stage-cd3

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ module "ecr" {
 }
 module "app_secrets" {
   source      = "./modules/secrets"
-  secret_name = "${local.prefix}-db-pass-v27"
+  secret_name = "${local.prefix}-db-pass-v28"
   tags        = local.common_tags
 }
 module "kubernetes" {
