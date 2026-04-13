0b427ff251d24cb97c6d0014afbe3946cc715acc
Noam-vm <noamjorno@gmail.com>
2026-04-13 23:10:35 +0300

    add & fix oidc5

--- terraform/oidc.tf
+++ terraform/oidc.tf
@@ -29,6 +29,16 @@ resource "aws_eks_access_policy_association" "github_runner_admin" {
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
   principal_arn = module.github_oidc_role.arn
 
+resource "aws_security_group_rule" "allow_eks_api_public" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.kubernetes.cluster_primary_security_group_id
+  description       = "Allow public access to EKS API for GitHub OIDC"
+}
+
   access_scope {
     type = "cluster"
   }
