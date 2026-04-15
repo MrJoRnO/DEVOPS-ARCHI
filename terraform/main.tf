c1bf2a53fa4d1879816f40da9071ef2a1da99802
Noam-vm <noamjorno@gmail.com>
2026-04-16 00:02:03 +0300

    prod1

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ module "ecr" {
 }
 module "app_secrets" {
   source      = "./modules/secrets"
-  secret_name = "${local.prefix}-db-pass-v29"
+  secret_name = "${local.prefix}-db-pass-v30"
   tags        = local.common_tags
 }
 module "kubernetes" {
