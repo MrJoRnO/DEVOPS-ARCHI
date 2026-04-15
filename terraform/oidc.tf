3b6dca4e70360e01dc6167f78d51d41e054e3d09
Noam-vm <noamjorno@gmail.com>
2026-04-16 02:39:02 +0300

    fix oidc4

--- terraform/oidc.tf
+++ terraform/oidc.tf
@@ -1,35 +1,33 @@
+module "github_oidc_provider" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
+  version = "~> 5.0"
+}
+
 module "github_oidc_role" {
-  count   = terraform.workspace == "stage" ? 1 : 0
   source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-role"
   version = "~> 5.0"
 
   name = "github-actions-eks-deployer"
-  
-  audience = "sts.amazonaws.com"
-  
-  subjects = ["repo:MrJoRnO/CloudRoute:*"]
-  
+
+  subjects = [
+    "repo:MrJoRnO/CloudRoute:*"
+  ]
+
   policies = {
     Admin = "arn:aws:iam::aws:policy/AdministratorAccess"
   }
 }
 
-data "aws_iam_role" "github_role" {
-  name = "github-actions-eks-deployer"
-
-  depends_on = [module.github_oidc_role]
-}
-
 resource "aws_eks_access_entry" "github_runner" {
-  cluster_name  = module.kubernetes.cluster_name
-  principal_arn = data.aws_iam_role.github_role.arn
-  type          = "STANDARD"
+  cluster_name      = module.kubernetes.cluster_name 
+  principal_arn     = module.github_oidc_role.arn
+  type              = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "github_runner_admin" {
   cluster_name  = module.kubernetes.cluster_name
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn = data.aws_iam_role.github_role.arn
+  principal_arn = module.github_oidc_role.arn
 
   access_scope {
     type = "cluster"
